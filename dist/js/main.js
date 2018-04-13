79af3b0495c8517596784ee5fad7e1d6ca8530df
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2018-04-13 16:56:21 +0300

    Slider

---
+++ dist/js/main.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper('.swiper-container', {
+  loop: true,
+  //effect: 'fade',
+  speed: 600,
+  //autoplay: {
+  //delay: 5000,
+  //},
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  }
+});
